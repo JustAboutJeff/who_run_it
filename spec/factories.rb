61013e48e3a75d05407d74838c78c817c3d158c9
Blurredbits <mmorris00@mac.com>
2013-07-04 15:22:46 -0500

    Initial Event integration test

--- spec/factories.rb
+++ spec/factories.rb
@@ -6,4 +6,15 @@ FactoryGirl.define do
     email "niles@nilesrocks.com"
     cellphone "222-222-2222"
   end
+
+  factory :event do
+    user_id "1"
+    title "Lincoln Park Run"
+    description "Easy run along lakeshore drive"
+    location_id "1"
+    start_time "1300"
+    pace "7"
+    url_key "#"
+  end
+
 end
