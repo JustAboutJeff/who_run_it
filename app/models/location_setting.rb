376261ca21a0311e05e558fc8a04714b0b9886c5
Blurredbits <mmorris00@mac.com>
2013-07-06 17:53:09 -0500

    Add location WIP

--- app/models/location_setting.rb
+++ app/models/location_setting.rb
@@ -1,5 +1,5 @@
 class LocationSetting < ActiveRecord::Base
-  attr_accessible :user_id, :notification_frequency, :notification_method, :latitude, :longitude
-  
+  attr_accessible :user_id, :name, :notification_frequency, :notification_method, :latitude, :longitude
+
   belongs_to :user
 end
