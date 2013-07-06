018df04f14086c2ef8f8e746e9c98a44da718f03
Blurredbits <mmorris00@mac.com>
2013-07-06 16:46:15 -0500

    Add location setting resource to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 WhoRunIt::Application.routes.draw do
   # Root
   root to: 'users#new'
-  
+
   # Users
   resources :users
   # Events
   resources :events
   # Sessions
   resources :sessions, only: [:new, :create, :destroy]
+  # Location Settings
+  resources :location_settings
 
   match '/signup',  to: 'users#new'
   match '/login',   to: 'sessions#new'
