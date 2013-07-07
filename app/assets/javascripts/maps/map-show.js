3bc144ed6a90fa91c2a2ee4ed355c3d62a833c10
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-06 19:47:26 -0500

    route tracer starts at the first waypoint

--- app/assets/javascripts/maps/map-show.js
+++ app/assets/javascripts/maps/map-show.js
@@ -1,15 +1,19 @@
 $(document).ready(function() {
-
   if ($('#map-show').length) {
-  var routeLayer = L.layerGroup();
-  var mapShow = L.mapbox.map('map-show', 'mthoover1.map-nnfbdcbz', {layers: routeLayer})
-  mapShow.fitBounds(waypoints);
+    var routeLayer = L.layerGroup();
+    var mapShow = L.mapbox.map('map-show', 'mthoover1.map-nnfbdcbz', {layers: routeLayer})
+    mapShow.fitBounds(waypoints);
 
-  var line = L.polyline(waypoints), animatedMarker = L.animatedMarker(line.getLatLngs());
-  mapShow.addLayer(animatedMarker);
+    var route = L.polyline(waypoints).addTo(mapShow);
+    var start = L.marker(waypoints[0]).addTo(mapShow);
+    var finish = L.marker(waypoints.slice(-1)[0]).addTo(mapShow);
 
-  var route = L.polyline(waypoints).addTo(mapShow);
-  var start = L.marker(waypoints[0]).addTo(mapShow);
-  var finish = L.marker(waypoints.slice(-1)[0]).addTo(mapShow);
+    animatedWaypoints = [waypoints[0]];
+    for (i = 0; i < waypoints.length; i++) {
+      animatedWaypoints.push(waypoints[i]);
+    }
+
+    var line = L.polyline(animatedWaypoints), animatedMarker = L.animatedMarker(line.getLatLngs());
+    mapShow.addLayer(animatedMarker);
   }
 });
