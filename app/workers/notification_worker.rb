3ac247083fa29b41adc1e0a97d879e66b03201b7
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-06 19:27:33 -0500

    location based notifications work

--- app/workers/notification_worker.rb
+++ app/workers/notification_worker.rb
@@ -4,8 +4,19 @@ class NotificationWorker
 
   def perform(event_id)
     event = Event.find(event_id)
+    event_coords = [event.waypoints.first.latitude, event.waypoints.first.longitude]
+
     User.all.each do |user|
-      NotificationMailer.notify(user,event).deliver
+      @sent = false
+      user.location_settings.each do |location|
+        break if @sent == true
+        location_coords = [location.latitude, location.longitude]
+        if Geocoder::Calculations.distance_between(event_coords, location_coords) <= 10
+          NotificationMailer.notify(user,event).deliver
+          @sent = true
+        end
+      end
     end
+
   end
 end
