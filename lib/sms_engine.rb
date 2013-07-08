0466ed67de075f3714b0a4ae8ef855b4d10c2351
Blurredbits <mmorris00@mac.com>
2013-07-08 18:48:50 -0500

    changed the text message to show the proper routing

--- lib/sms_engine.rb
+++ lib/sms_engine.rb
@@ -8,8 +8,8 @@ class SmsEngine
     account_sid = ENV['TWILIO_SID']
     auth_token = ENV['TWILIO_AUTH_TOKEN']
     @client = Twilio::REST::Client.new account_sid, auth_token
-      
-    message = @client.account.sms.messages.create(:body => "New WhoRun.It Event In Your Area! #{@event.route.distance} miles at #{@event.pace} min/mile. #{@event.start_time.localtime.strftime("%I:%M%p")}. whorunit.herokuapp.com/#{@event.url_key}",
+
+    message = @client.account.sms.messages.create(:body => "New WhoRun.It Event In Your Area! #{@event.route.distance} miles at #{@event.pace} min/mile. #{@event.start_time.localtime.strftime("%I:%M%p")}. whorunit.herokuapp.com/events/#{@event.url_key}",
         :to => @user.cellphone,
         :from => ENV['TWILIO_PHONE_NUMBER'])
     puts message.sid
