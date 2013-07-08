75ef128f4354843d90a2e28e7190803c5bbcfb8b
Matthew Hoover <Matt@MattBookAir.local>
2013-07-07 19:47:57 -0500

    notification worker checks for contact method prefs

--- app/workers/notification_worker.rb
+++ app/workers/notification_worker.rb
@@ -7,16 +7,26 @@ class NotificationWorker
     event_coords = [event.waypoints.first.latitude, event.waypoints.first.longitude]
 
     User.all.each do |user|
-      @sent = false
+      sent_email = false
+      sent_text = false
       user.location_settings.each do |location|
-        break if @sent == true
         location_coords = [location.latitude, location.longitude]
         if Geocoder::Calculations.distance_between(event_coords, location_coords) <= 10
-          NotificationMailer.notify(user,event).deliver
-          @sent = true
+          
+          if (location.notification_method == 2) || (location.notification_method == 4)
+            break if sent_email == true
+            NotificationMailer.notify(user,event).deliver
+            sent_email = true
+          end
+
+          if (location.notification_method == 3) || (location.notification_method == 4)
+            break if sent_text == true
+            # send text
+            sent_text = true
+          end
+
         end
       end
     end
-
   end
 end
