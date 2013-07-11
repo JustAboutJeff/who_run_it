44980cd134ffce89cae4f1155eccf582140a2d2d
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-11 10:03:37 -0500

    update data display on location_setting view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def notification_method(id)
+    methods = {
+      "1" => "None",
+      "2" => "Email",
+      "3" => "Text Message",
+      "4" => "Text & Email"
+    }
+    return methods[id]
+  end
 end
